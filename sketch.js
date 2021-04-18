e3e7a578cca5864c01e71d92e933cc5ee7575684
meghana2109 <megha.manjunath11@gmail.com>
2021-04-18 11:14:21 +0530

    Add Platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var paltform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(100,300,200,300);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -53,6 +55,7 @@ function draw(){
     box4.display();
     pig3.display();
     log3.display();
+    platform.display();
 
     box5.display();
     log4.display();
